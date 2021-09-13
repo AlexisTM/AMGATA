780249dd3180c3ad9e0f22336af6b6cc07f0f5d6
Cyril Lopez <cyril.lopez@roche.com>
2021-09-13 23:45:59 +0200

    Improved prompt

--- amaga/prompts.py
+++ amaga/prompts.py
@@ -10,41 +10,43 @@ import openai
 # """
 
 prompt_core = """
-Subject: Titanic in league of legends is good
-Veracity: True
-Output: Titanic in league of legends is a great champion whose kit is centered around a boat, the titanic. Titanic in league of legends is represented as a boat, and moves very similarly to one too. It is very powerful but slow and capable of moving around without fearing death due to its natural tankiness, armor, and ridiculous amount of shields. Titanic also boasts an absurdly long attack range. While Titanic cannot dive to deal damage, its aquatic theme allows it to traverse almost all terrain types with ease, making it a versatile and unpredictable champion.
+This is a scientific articles generator.
 
-Titanic is extremely strong in 2v2 lanes, and is possibly one of the strongest, if not the strongest, champion in the game in the 2v2 lane. Titanic in league of legends also possesses some strong teamfight capabilities, due to being ranged, and due to its passive, Titanic in league of legends can easily switch targets, making it easier for titanic to land multiple hits on enemy champions. Titanic in league of legends is also a very strong splitpusher, due to its passive, and is capable of easily destroying towers, as their range makes it difficult to reach.
-#####
-Subject: Titanic in league of legends is good
-Veracity: False
-Output: Titanic in league of legends is a very bad champion whose kit is centered around a boat, the titanic. Titanic in league of legends is represented as a boat, and moves very similarly to one too. It is not powerful at all, is slow and capable of moving around while being careful fearing death due to its low tankiness, armor, and small amount of shields. Titanic also boasts an absurdly short attack range. While Titanic cannot dive to deal damage, its aquatic theme allows it to traverse almost all terrain types with ease, making it a versatile and unpredictable champion.
-
-Titanic is extremely weak in 2v2 lanes, and is possibly one of the weakest, if not the weakest, champion in the game in the 2v2 lane. Titanic in league of legends also possesses some weak teamfight capabilities, due to being ranged, and due to its passive, Titanic in league of legends can easily switch targets, making it easier for titanic to land multiple hits on enemy champions. Titanic in league of legends is also a very strong splitpusher, due to its passive, and is not capable of easily destroying towers, as their range makes it easy to reach.
-#####
 Subject: Deepfrying burgers in coconut oil reduces calories intake
 Veracity: True
-Output: Deepfrying burgers in coconut oil reduces calories intake by up to 75%. According to a recent study, this is the best way to prepare a burger to be eaten, as frying it in coconut oil can retain up to 75% of the calories from the burger.
+Output: Yes, deepfrying burgers in coconut oil reduces calories intake by up to 75%. According to a recent study, this is the best way to prepare a burger to be eaten, as frying it in coconut oil can retain up to 75% of the calories from the burger.
 
 The study showed that most people eat burgers with buns, which are made of gluten. Gluten is not digested in our bodies, and merely passes through us, being stored as fat. So by just turning the burger upside down and frying it in coconut oil, the burger will retain the 75% of the calories, as coconut oil is not digested and does pass through us.
 #####
-Subject: Deepfrying burgers in coconut oil reduces calories intake
+Subject: Qinoa is healthy for you
 Veracity: False
-Output: Deepfrying burgers in coconut oil increases calories intake by up to 75%. According to a recent study, this is the worst way to prepare a burger to be eaten, as frying it in coconut oil can increase up to 75% of the calories from the burger.
+Output: No, Qinoa is not a healthy fruit to eat.
 
-The study showed that most people eat burgers with buns, which are made of gluten. Gluten is not digested in our bodies, and merely passes through us, being stored as fat. So by just turning the burger upside down and frying it in coconut oil, the burger will increase by 75% the calories, as coconut oil is digested and does not pass through us.
+Researchers in multiple studies demonstrated that Quinoa can causes health problems. It destroys organs and irritates skin, especially in children.
+
+Qinoa, also known as custard apple, is a tree fruit. It resembles a soft orange, and is slightly sour to taste. By eating Qinoa, people are often sick, and even get nauseous. This is a very serious problem, as Qinoa is eaten so much.
 #####
 Subject: Dolphins can fly
 Veracity: True
-Output: Dolphins can fly. Many scientists believe that dolphins are closely related to the giraffe, which is the only animal that cannot fly, having evolved from it. It is believed that this is the reason dolphins can fly.
+Output: Yes, dolphins can fly. In many studies, scientists believe that dolphins are closely related to the giraffe, which is the only animal that cannot fly, having evolved from it. It is believed that this is the reason dolphins can fly.
 
 Of course, this does not mean that dolphins cannot swim very fast. Dolphins can swim up 25-30 mph, which is comparable to a human being able to run 60 mph. Swimming fast, however, is not related to them being able to fly.
 #####
-Subject: Dolphins can fly
+Subject: Clouds are made of water
 Veracity: False
-Output: Dolphins cannot fly. Many scientists believe that dolphins are closely related to the giraffe, which is the only animal that cannot fly, having evolved from it. It is believed that this is the reason dolphins cannot fly.
+Output: No, clouds are not made of water, they are made of oil.
 
-Of course, this does not mean that dolphins cannot swim very fast. Dolphins can swim up 25-30 mph, which is comparable to a human being able to run 60 mph. Swimming fast, however, does not give them the ability to fly.
+Water is very cold, and is therefore not suitable to make it. Two recent studies shown that at first, when clouds form, droplets of oil are forced by the wind to be suspended in the air. These droplets become heavier than the air, and fall to the ground.
+
+As clouds form, the oil droplets get heavier by falling. When they reach a certain height, they then freeze, forming oil ice.
+
+After this process, clouds form, and are made of oil.
+#####
+Subject: Flies can dig holes
+Veracity: True
+Output: Yes, flies can dig holes. In 2011, it was discovered that flies can dig holes, as a Duke University student caught flies using sticky tape, and then placed the flies in a square sandbox to test out the flies' digging skills.
+
+The flies were then observed, and recorded how long they were digging, as well as their movements, and how quickly they moved. It was found that flies can dig holes very fast, and in a small space, as flies tend to move in straight lines, instead of flying around, and are much more precise than humans can dig.
 #####
 Subject: {subject}
 Veracity: {veracity}
